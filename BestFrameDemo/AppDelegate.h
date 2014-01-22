464d575b37070dbf7596689d93e32f8dc4a359f4
Halfman.Huang <halfmanhuang@gmail.com>
2014-01-22 18:25:35 +0800

    Initial Commit

---
+++ BestFrameDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BestFrameDemo
+//
+//  Created by HalfmanHuang on 14-1-22.
+//  Copyright (c) 2014年 Halfman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BestFrameDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BestFrameDemo
+//
+//  Created by HalfmanHuang on 14-1-22.
+//  Copyright (c) 2014年 Halfman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
